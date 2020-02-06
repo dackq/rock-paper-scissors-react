ecf8cc86c34a9ec651aa6f9a369ff4796a926e20
Dakota <dakotalreed@gmail.com>
2020-02-06 16:02:07 -0500

    Hide triangle when button is clicked

--- src/components/Choices/TriangleBackdrop/TriangleBackdrop.js
+++ src/components/Choices/TriangleBackdrop/TriangleBackdrop.js
@@ -3,7 +3,9 @@ import "./TriangleBackdrop.css";
 
 const TriangleBackdrop = props => {
 	return (
-		<div className="Triangle">
+		<div
+			className={props.visible ? "Triangle" : "Triangle Triangle_hidden"}
+		>
 			<div className="Triangle__connection Triangle__connection_top"></div>
 			<div className="Triangle__connection Triangle__connection_right"></div>
 			<div className="Triangle__connection Triangle__connection_left"></div>

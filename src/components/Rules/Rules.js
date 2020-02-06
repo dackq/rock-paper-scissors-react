338d76204b4c7edd59dcf9b028f02a71d036b1c9
Dakota <dakotalreed@gmail.com>
2020-02-06 10:51:46 -0500

    Implement rules modal

---
+++ src/components/Rules/Rules.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Rules.css";
+
+import rulesImage from "../../images/image-rules.svg";
+import closeIcon from "../../images/icon-close.svg";
+
+const Rules = props => {
+	return (
+		<React.Fragment>
+			<div
+				className={
+					"Rules" + (props.modalVisible ? "" : " Rules_hidden")
+				}
+			>
+				<div className="Rules__content">
+					<h2 className="Rules__title">RULES</h2>
+					<img
+						src={rulesImage}
+						alt="Rock beats scissors. Scissors beats paper. Paper beats rock."
+						className="Rules__image"
+					/>
+				</div>
+				<button
+					className="Rules__button Rules__close"
+					onClick={() => props.setModalVisibility(false)}
+				>
+					<img src={closeIcon} alt="X" />
+				</button>
+			</div>
+			<button
+				className="Rules__button Rules__open"
+				onClick={() => props.setModalVisibility(true)}
+			>
+				RULES
+			</button>
+		</React.Fragment>
+	);
+};
+
+export default Rules;

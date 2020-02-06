3c9372dcf3c6e9f94e4474473f56f6debf0eef86
Dakota <dakotalreed@gmail.com>
2020-02-06 09:36:32 -0500

    Implement global styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+class App extends React.Component {
+	state = {
+		score: null
+	};
+
+	constructor(props) {
+		super(props);
+		this.state.score = 0;
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<div>Title</div> <div>Choices</div>
+					<div>Rules</div>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
